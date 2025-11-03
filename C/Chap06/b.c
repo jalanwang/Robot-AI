8dcd49cee047773f9bf7138fbde4baa32b8fcd3a
jalanwang <jalanwang@gmail.com>
2025-11-03 11:57:39 +0900

    Add b.c to print numbers from 10 to 1

---
+++ C/Chap06/b.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    //10부터 1까지 출력하는 프로그램을 작성하시오.
+    int i=10;
+    
+    while (i>0) printf("i: %d\n", i--);
+    
+    return 0;
+}
