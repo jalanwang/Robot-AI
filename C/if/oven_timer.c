d32211b2d219a3caf64b340c55c8383407af25dc
jalanwang <jalanwang@gmail.com>
2025-11-06 09:16:05 +0900

    Add oven timer program in oven_timer.c
    24시로 표현되는 문제가 있음. 수정요.

---
+++ C/if/oven_timer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    
+    int cur_hour, cur_min; //현재 시간
+    int operating_min; //동작 분
+    
+    int end_hour, end_min; //종료 시간
+    
+    int const mod_hour=24; //시간 단위
+    int const mod_min=60; //분 단위
+    
+    int total_min;
+    
+    scanf("%d %d", &cur_hour, &cur_min);
+    scanf("%d", &operating_min);
+    
+    total_min=cur_hour*mod_min+cur_min+operating_min;
+    
+    end_hour=total_min/mod_min;
+    if(end_hour> 23) end_hour-mod_hour;
+    end_min=total_min%mod_min;
+    
+    printf("%d %d", end_hour, end_min);
+    
+    return 0;
+}
