f3a41a8ee63fb152b4c4e3f9dd31b7d1dba633ec
jalanwang <jalanwang@gmail.com>
2025-11-10 11:27:08 +0900

    Add reverse basket game functionality
    Implement a basket game that reverses the order of elements in specified ranges based on user input.

---
+++ C/basket game/rev_basket_game.c
@@ -0,0 +1,57 @@
+//basket game
+//reverse order of basket in specific range
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    int rev_times;
+    int start, end;
+    int temp;
+    
+    scanf("%d %d", &N, &rev_times);
+    int basket[N];
+    int r_basket[N];
+    
+    for(int i=0;i<N;i++) 
+    {
+        basket[i]=i+1; //N+1-> N번째
+        r_basket[i]=i+1; //복사본
+    }
+
+        // for(int k=0;k<N;k++)
+        // {
+        //     printf("%2d", r_basket[k]);
+        // }
+        // printf("\n");
+        
+    for(int i=0;i<rev_times;i++)
+    {
+        int start, end;
+        scanf("%d %d", &start, &end);    
+    
+        for(int j=start; j<start+(end-start)/2.0; j++) //시작 + 갯수/2
+        {
+            temp=r_basket[j-1];
+            r_basket[j-1]=basket[(end-1-(j-start))];
+            r_basket[(end-1-(j-start))]=temp; //1, 4
+            //r_basket[0]=basket[3] r_basket[1-1]=basket[4-1]
+            //r_basket[1] end=[2] r_basket[2-1]=basket[4-1-(j-start)]
+            //r_basket[1] end=[3]
+            //r_basket[2] end=[2] --> stop point
+            //r_basket[3] end=[1]
+            //r_basket[4] end=[0]
+        }
+        
+        // for(int k=0;k<N;k++)
+        // {
+        //     printf("%2d", r_basket[k]);
+        // }
+        // printf("\n");
+    }
+
+    for(int i=0;i<N;i++) printf("%2d", r_basket[i]);
+    
+    return 0;
+}
