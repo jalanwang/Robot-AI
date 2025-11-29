9cf7ceab3dfc92a949686f3b1c4547f8a4471538
jalanwang <jalanwang@gmail.com>
2025-11-30 08:02:45 +0900

    ;;

--- C/nocode/181889.c
+++ C/nocode/181889.c
@@ -14,7 +14,6 @@ int* solution(int num_list[], size_t num_list_len, int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(n*sizeof(int)); //
     for(int i=0;i<n;i++) { //
-    for(int i=0;i<n;i++) { //
         answer[i]=num_list[i]; // 첫 n개 원소 복사    
     }
     return answer;
