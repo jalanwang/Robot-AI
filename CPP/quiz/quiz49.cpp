5405b9fd90d3807820d255312bb66ab1a026ceae
jalanwang <jalanwang@gmail.com>
2025-12-02 10:18:52 +0900

    Car 클래스의 빌더패턴 구현 개선: 코드 스타일 정리 및 가독성 향상

--- CPP/quiz/quiz49.cpp
+++ CPP/quiz/quiz49.cpp
@@ -7,7 +7,8 @@ inner class형태의 빌더패턴 또는 블로치(Bloch)빌더로 불리는 가
 #include <iostream>
 #include <memory>
 
-class Car {
+class Car
+{
 
 private:
     std::string mBrand;
@@ -15,63 +16,70 @@ private:
     std::string mColor;
 
     // 생성자를 private로
-    Car(const std::string& brand, int speed, const std::string& color)
+    Car(const std::string &brand, int speed, const std::string &color)
         : mBrand(brand), mSpeed(speed), mColor(color) {}
 
 public:
-    //빌더패턴 사용 클래스 시작
-    class Builder { 
+    // 빌더패턴 사용 클래스 시작
+    class Builder
+    {
     private:
-        std::string mBrand="Genesis";
-        int mSpeed=300;
-        std::string mColor="White";    
+        std::string mBrand = "Genesis";
+        int mSpeed = 300;
+        std::string mColor = "White";
 
     public:
-        Builder& setBrand(const std::string& brand) {
-        mBrand=brand;
-        return *this;
-    }
-    Builder& setSpeed(const int speed) {
-        mSpeed=speed;
-        return *this;
-    }
-    Builder& setColor(const std::string& color) {
-        mColor=color;
-        return *this;
-    }
-    
-    Car build() const {
-        return Car(
-            mBrand, mSpeed, mColor
-        );
-    }
-    }; //Builder Class 종료
+        Builder &setBrand(const std::string &brand)
+        {
+            mBrand = brand;
+            return *this;
+        }
+        Builder &setSpeed(const int speed)
+        {
+            mSpeed = speed;
+            return *this;
+        }
+        Builder &setColor(const std::string &color)
+        {
+            mColor = color;
+            return *this;
+        }
 
-    //getter
-    std::string getBrand() { 
-        return mBrand; 
-    }
-    int getSpeed() { 
-        return mSpeed; 
-    }
-    std::string getColor() { 
-        return mColor; 
-    }
-}; //Car Class 종료
+        Car build() const
+        {
+            return Car(
+                mBrand, mSpeed, mColor);
+        }
+    }; // Builder Class 종료
 
-int main() {
+    // getter
+    std::string getBrand()
+    {
+        return mBrand;
+    }
+    int getSpeed()
+    {
+        return mSpeed;
+    }
+    std::string getColor()
+    {
+        return mColor;
+    }
+}; // Car Class 종료
 
- Car genesis = Car::Builder()
-                        .setBrand("제너시스")
-                        .setSpeed(300)
-                        .setColor("Pearl White")                        
-                        .build();   
+int main()
+{
+
+    Car genesis = Car::Builder()
+                      .setBrand("제너시스")
+                      .setSpeed(300)
+                      .setColor("Pearl White")
+                      .build();
 
     std::cout << "==== Car Info ====\n";
     std::cout << "Brand: " << genesis.getBrand() << std::endl;
-    std::cout << "Speed: " << genesis.getSpeed() << std::endl;
+    std::cout << "Speed: " << genesis.getSpeed() << "km/hour" << std::endl;
     std::cout << "Color: " << genesis.getColor() << std::endl;
-    
-    return 0;
 
+    return 0;
 }
