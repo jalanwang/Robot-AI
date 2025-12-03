e682c1cf90d7fbf22c9fb6c1806394d96987155c
jalanwang <jalanwang@gmail.com>
2025-12-03 10:34:43 +0900

    Parent 및 Child 클래스 추가: 다형성 예제 구현

---
+++ CPP/cpp7/Ex9-1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+
+class Parent {
+public:
+	virtual void f() { cout << "Parent::f() called" << endl; }
+};
+
+class Child : public Parent {
+public:
+	void f() override { cout << "Child::f() called" << endl; }
+};
+
+int main() {
+	unique_ptr<Child> child = make_unique<Child>();
+    child->f();
+
+    // Derived d;
+    // Derived* pDer;
+	// pDer = &d; //객체의 시작주소를 pDer에 대입
+	// pDer->f(); // Derived::f() 호출 //
+ 
+    unique_ptr<Parent> parent = make_unique<Child>();
+    parent->f();
+
+	// Base* pBase;
+	// pBase = pDer; // 업캐스팅
+	// pBase->f(); // Base::f() 호출
+
+    return 0;
+}
