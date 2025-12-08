7aeb50de61729bd03c97cbee87ef71db74bb2fa5
jalanwang <jalanwang@gmail.com>
2025-12-08 12:33:28 +0900

    list_ex1.py 수정: 잘못된 remove() 사용에 대한 주석 추가

--- Python/python1/list_ex1.py
+++ Python/python1/list_ex1.py
@@ -3,6 +3,8 @@ for element in array:
     if str(element).isdigit(): 
         print(element)
     else: 
-        element.remove()
+        element.remove() #remove는 값을 찾아서 변경하는거야. 그래서 틀렸네. 
+                            #다른 방법을 찾아봐
+                            
 
 
