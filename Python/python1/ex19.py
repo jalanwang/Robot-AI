202cb4de509b0ccccc38f7af7849197ccb4c0814
jalanwang <jalanwang@gmail.com>
2025-12-09 08:35:04 +0900

    ex19.py 파일 수정: 최대 곱셈 값을 찾는 로직 개선 및 불필요한 코드 제거

--- Python/python1/ex19.py
+++ Python/python1/ex19.py
@@ -2,11 +2,24 @@
 #최대가 되는 경우는 어떤 숫자를 곱했을 때인지를 찾아 주세요
 
 max_value=0
-i=0
-j=0
-for i in range(1, 100, 1):
-    for j in reversed(range(1, 100,1)):
-        if (max < int(i*j)):
-            max=i*j
+
+ordered_list=range(1,100,1)
+rev_list=list(reversed(ordered_list)) # list() 함수 사용. [reversed()]하면 안됨.
+
+result=[] #값 없이 초기화. append()로 넣어야 한다.
+
+# 이터레이터를 중복해서 사용하면서 나타난 2개의 문제점. ********
+# 1. 인덱스 범위가 맞지 않음.
+# 2. 원래 이터레이터로 2개의 리스트에 동시 접근이 안됨
+# for it in ordered_list:
+#     result.append(it)=it*rev_list[it]
+#     if max_value < result[it]:
+#         max_value=result[it]
          
-print("최대가 되는 경우: {} * {} = {}".format(i,j,max_value))
+for i in range(len(ordered_list)):
+    result.append(ordered_list[i]*rev_list[i])
+    if max_value<result[i]:
+        max_value = result[i]
+        max_idx=i+1 #인덱스값 +1이 실재 숫자값
+
+print("최대가 되는 경우: {} * {} = {}".format(max_idx,100-max_idx,max_value))
