2bc828afa204dc5f375e4aa080adb0d5e3bc327e
jalanwang <jalanwang@gmail.com>
2025-12-09 08:02:30 +0900

    ex17.py 파일에 주석 추가: 딕셔너리 구성 방법 설명

--- Python/python1/ex17.py
+++ Python/python1/ex17.py
@@ -1,3 +1,5 @@
+#아래의 리스트를 딕셔너리로 구성하시오.
+
 key_list=["name", "hp", "mp", "level"]
 value_list=["기사", 200, 30, 5]
 
@@ -9,3 +11,15 @@ for key in key_list:
     i+=1
 
 print(character)
+
+# another solution
+# key_list = ['name', 'hp', 'mp', 'level']
+# value_list = ['기사', 200, 30, 5]
+# character = {}
+
+# for i in range(len(key_list)):
+#     for j in range(len(value_list)):
+#         if i == j:           
+#             character[key_list[i]] = value_list[j]
+
+# print(character)
