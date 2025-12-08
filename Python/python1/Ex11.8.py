6ddc73f813de7b4d42e4bfc35494b5d6a28cc0ad
jalanwang <jalanwang@gmail.com>
2025-12-08 16:14:07 +0900

    Ex11.8.py 파일 추가: 7D 건조 망고 정보 출력 로직 구현

---
+++ Python/python1/Ex11.8.py
@@ -0,0 +1,10 @@
+dct= {
+    "name": "7D 건조 망고",
+    "type": "당절임",
+    "ingredient": ["망고", "설탕", "메타중아황 산나트륨", "치자황색소"],
+    "origin": "필리핀"
+}
+
+for key in dct:
+    print(key, " : ", dct[key])
+    
