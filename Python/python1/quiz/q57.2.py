10b6b6e1b690d3ea8094021c0d7a9afc5408317c
jalanwang <jalanwang@gmail.com>
2025-12-08 14:19:28 +0900

    q57.2.py 파일 수정: 이중 반복을 이용하여 2차원 리스트의 원소 합계를 구하는 로직으로 변경

--- Python/python1/quiz/q57.2.py
+++ Python/python1/quiz/q57.2.py
@@ -1,11 +1,14 @@
 #원소의 갯수를 구하시오.
 #단 for 이중반복으로 푸시오.
 
-cnt=0
+# 2차원 리스트의 원소의 합계를 구하세요.
+# 단, for 이중반복 구문을 꼭 사용하세요.
+
+sum=0
 array=[[1,2,3], [4,5,6], [7,8,9]]
 for it_row in array:
-    for it_col in array:
-        cnt+=1
+    for it_col in it_row:
+        sum+=it_col
 
-print(cnt)
+print(sum)
 
