77e9bd556eb2a4b21509d7a60839314e03333fe0
jalanwang <jalanwang@gmail.com>
2025-12-10 08:06:35 +0900

    dic01.py 파일 수정: 주석 처리된 코드 제거 및 새로운 딕셔너리 정의, 출력 형식 변경

--- Python/python1/dic01.py
+++ Python/python1/dic01.py
@@ -1,13 +1,24 @@
-dictionary= {
-    "name": "7D 건조 망고",
+# dictionary= {
+#     "name": "7D 건조 망고",
+#     "type": "당절임",
+#     "ingredient": ["망고", "설탕", "메타중아황 산나트륨", "치자황색소"],
+#     "origin": "필리핀"
+# }
+
+# print("제품이름 : ", dictionary["name"])
+# print("타입: ", dictionary["type"])
+
+# dictionary['name']="8D 건조 망고"
+# print("제품이름 : ", dictionary["name"])
+
+diction = {
+    "name":"7D 건조 망고",
     "type": "당절임",
-    "ingredient": ["망고", "설탕", "메타중아황 산나트륨", "치자황색소"],
-    "origin": "필리핀"
+    "ingredient": ["망고", "설탕", "메타중아황산 나트륨", "치자황색소"],
+    "origin": "말레이시아"
 }
 
-print("제품이름 : ", dictionary["name"])
-print("타입: ", dictionary["type"])
-
-dictionary['name']="8D 건조 망고"
-print("제품이름 : ", dictionary["name"])
+print(f"제품이름:{diction["name"]}")
+print(f"타입: {diction["당절임"]}")
+print(f"성분: {diction["ingredient"]}")
 
