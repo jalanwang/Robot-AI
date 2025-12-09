14f81ce11113413a82c556dcbcd78ac8d2b10552
jalanwang <jalanwang@gmail.com>
2025-12-09 11:20:07 +0900

    Ex10.8.py 파일 추가: 컴퓨터 사양 정보를 출력하는 로직 구현

---
+++ Python/python1/Ex10.8.py
@@ -0,0 +1,11 @@
+computer={}
+
+computer['cpu']='Intel i7'
+computer['ram']='DDR 5 64G'
+computer['GPU']='Nvidia 5090 TI'
+
+print(computer)
+print(computer.keys())
+print(computer.values())
+
+print(computer['cpu'])
