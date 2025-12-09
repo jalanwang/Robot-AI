fa58b73d5ce15ffa080226d0797fbb9a81378f67
jalanwang <jalanwang@gmail.com>
2025-12-10 08:45:12 +0900

    리스트에서 문자열 제거 기능 구현: 새로운 리스트에 문자열이 아닌 요소만 추가

--- Python/python1/list_ex1.py
+++ Python/python1/list_ex1.py
@@ -1,10 +1,15 @@
+# 문자열을 리스트에서 제거하기. 단, 새로운 리스트로 만든다.
+
+
 array=[1,2,3,"say hello", 5]
+new_array = []
+
 for element in array:
-    if str(element).isdigit(): 
-        print(element)
-    else: 
-        element.remove() #remove는 값을 찾아서 변경하는거야. 그래서 틀렸네. 
-                            #다른 방법을 찾아봐
+    # 타입이 문자열(str)이 아닌 경우만 새 리스트에 추가
+    if type(element) != str:
+        new_array.append(element)
+
+print(new_array)
                             
 
 
