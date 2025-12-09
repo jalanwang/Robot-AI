c30e5ecdc32fa7b11ac7079cd430e8004b684990
jalanwang <jalanwang@gmail.com>
2025-12-09 10:03:58 +0900

    Ex18.py 파일 추가: 1~10까지의 3의 배수 출력 로직 구현

---
+++ Python/python1/Ex18.py
@@ -0,0 +1,16 @@
+#1~10까지 출력하는데 3의 배수만 출력하세요.
+#단 continue를 사용해야 합니다.
+#루프는 while을 사용하세요.
+# 3 6 9
+i=1
+while i<11:
+    if (i%3)!=0:
+        continue
+
+    else:
+        print(i, end=' ')
+    
+    i+=1
+
+
+
