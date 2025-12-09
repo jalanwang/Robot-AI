559ad9df7d6a7f848dbccfabff2b13b773d6db79
jalanwang <jalanwang@gmail.com>
2025-12-09 12:02:50 +0900

    Ex25.py 파일 추가: 문자열을 지정한 횟수만큼 출력하는 함수 구현

---
+++ Python/python1/Ex25.py
@@ -0,0 +1,16 @@
+def print_3_times():
+    print("안녕하세요")
+    print("안녕하세요")
+    print("안녕하세요")
+
+print_3_times()
+
+print()
+
+def print_n_times(string1, times):
+    for i in range(times):
+        print(string1)
+
+print_n_times("반갑습니다", 3)
+print()
+print_n_times("반갑습니다", 5)
