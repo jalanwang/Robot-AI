71981cdc1b14bfd4e2186e38c51cf0af13c4de23
jalanwang <jalanwang@gmail.com>
2025-12-10 08:06:31 +0900

    date.py 파일 수정: 날짜 및 시간 출력 형식 변경 및 주석 처리된 코드 정리

--- Python/python1/date.py
+++ Python/python1/date.py
@@ -1,5 +1,12 @@
-import datetime
-now=datetime.datetime.now() #여기서 객체를 만든것이다.
+# import datetime
+# now=datetime.datetime.now() #여기서 객체를 만든것이다.
 
 #print("{}년{}월{}일".format(now.year, now.month, now.day))
-print(f"{now.year}년{now.month}월{now.day}일")
+# print(f"{now.year}년{now.month}월{now.day}일")
+
+
+
+
+import datetime
+now=datetime.datetime.now()
+print(f"{now.month} {now.day} {now.hour} {now.minute} {now.second}")
