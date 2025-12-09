c4bd7f124d409a37f26aa640c041acea723fd7c2
jalanwang <jalanwang@gmail.com>
2025-12-10 08:40:31 +0900

    주석 수정: 특정 값 제거하기에 대한 설명 추가

--- Python/python1/Ex13.py
+++ Python/python1/Ex13.py
@@ -1,4 +1,4 @@
-#해당하는 값 제거하기
+#해당하는 특정 값 제거하기
 
 list_test=[1,2,1,2]
 value=2
@@ -7,3 +7,14 @@ while value in list_test:
     list_test.remove(value)
 
 print(list_test)
+
+##########################################
+# filter함수를 써볼까?
+##########################################
+
+list_test1=[1,2,1,2,4,5,6,7,8]
+value=2 # 홀 짝을 구분짓기 위한 수
+
+odd_list=list(filter(lambda x: x%value!=0, list_test1))
+
+print(odd_list)
