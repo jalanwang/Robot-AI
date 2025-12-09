cc7224720e267b22780721f24ef8ef2b5e06fa99
jalanwang <jalanwang@gmail.com>
2025-12-10 08:23:36 +0900

    Ex10.8.py 파일 수정: 주석 추가 및 출력 형식 개선

--- Python/python1/Ex10.8.py
+++ Python/python1/Ex10.8.py
@@ -1,11 +1,16 @@
-computer={}
+computer={} #dictionary 초기화
 
-computer['cpu']='Intel i7'
+computer['cpu']='Intel i7' #입력 방법
 computer['ram']='DDR 5 64G'
 computer['GPU']='Nvidia 5090 TI'
 
 print(computer)
-print(computer.keys())
-print(computer.values())
+print(computer.keys()) #키만 출력
+                       #dict_keys(['cpu', 'ram', 'GPU'])
+print(type(computer.keys())) # 튜플인가?
+                             # <class 'dict_keys'> 별도 클라스라고 하는데?
+                             # 아니라고 함. 왜 보여줄 땐, 튜플처럼 보여줬을까?
+print(computer.values()) #값만 출력
 
-print(computer['cpu'])
+print(computer['cpu']) #키에 따른 값 출력
+
