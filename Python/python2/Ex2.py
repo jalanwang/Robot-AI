b3c4e3f46ee7c2d28ee23ba141d894aedb609f28
jalanwang <jalanwang@gmail.com>
2025-12-10 08:50:04 +0900

    주석 추가: map 함수와 람다 사용에 대한 설명 보강

--- Python/python2/Ex2.py
+++ Python/python2/Ex2.py
@@ -1,14 +1,14 @@
 str_numbers=['10', '20', '30', '40', '50']
-result=map(int, str_numbers)
+result=map(int, str_numbers) # map함수 설명
 print(list(result))
 
 numbers=[1,2,3,4,5]
 def func(x):
     return x*2
-
+result=map(func, numbers) # map함수에서 펑션에 대한 설명
+print(list(result))
 
 #### 이것이 람다다!!!!
-#result=map(func, numbers)
-result=map(lambda x: x*3, numbers)
+result=map(lambda x: x*3, numbers) # map 함수에 람다 사용. 람바다~~~~ 람바~~ 람람람
 
 print(list(result))
