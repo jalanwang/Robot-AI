2ec15f1711601e503f0b60ba32f4a1686f975947
jalanwang <jalanwang@gmail.com>
2025-12-10 10:49:00 +0900

    주석 처리: 파일 쓰기 예제 주석 처리 및 파일 읽기 기능 유지

--- Python/python2/Ex6.py
+++ Python/python2/Ex6.py
@@ -1,5 +1,9 @@
 #with 키워드
 
-with open('basic2.txt', 'w') as file:
-    file.write('Hello python programming...!')
+# with open('basic2.txt', 'w') as file:
+#     file.write('Hello python programming...!')
 
+with open('basic2.txt', 'r') as file:
+    rcvd=file.read()
+
+print(rcvd)
