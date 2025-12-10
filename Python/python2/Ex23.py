12766f3a7f6d4dd0824bc23275ab8fdd16e21558
jalanwang <jalanwang@gmail.com>
2025-12-11 08:17:12 +0900

    주석 추가 및 로또 번호 출력 형식 개선

--- Python/python2/Ex23.py
+++ Python/python2/Ex23.py
@@ -5,5 +5,6 @@ list_a = range(1,46)
 lucky = random.sample(list_a, k=7)
 lotto=sorted(lucky[0:6])
 bonus=lucky[-1]
+#print("로또번호:", *lotto, "보너스 번호:", bonus) 구문이 이상하다.
 
-print(f"로또번호: {*lotto,}, 보너스 번호: {bonus}")
+print("로또번호:", *lotto, "보너스 번호:", bonus)
