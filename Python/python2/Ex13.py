374dca79bb66d24031f379f18380af400744b440
jalanwang <jalanwang@gmail.com>
2025-12-10 12:40:13 +0900

    새 파일 추가: Ex13.py 파일 생성 및 숫자 추출 기능 구현

---
+++ Python/python2/Ex13.py
@@ -0,0 +1,17 @@
+#try exception
+
+list_input_a = ["52", "273", "32", "스파이", "103"]
+
+list_num=[]
+
+for item in list_input_a:
+    try:
+        float(item)
+        #list_num.append(item)
+    except:
+        pass
+    else:
+        list_num.append(item)
+        
+print(f"{list_input_a} 내부에 있는 숫자는", end=' ')
+print(f"{list_num}입니다.")
