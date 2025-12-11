8fefdf489ccea37436cc9b18982405efed70c6a3
jalanwang <jalanwang@gmail.com>
2025-12-11 09:33:26 +0900

    새 파일 추가: p106.cpp 파일 생성 및 입력 스트림에서 실수 벡터 읽기 기능 구현

---
+++ accelatedC++/p106.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+ #include <vector>
+
+ std::istream& read_hw(std::istream& in, std::vector<double>& hw) {
+    if(in) {  
+        hw.clear();
+       
+        double x;
+
+        while(in>>x)
+            hw.push_back(x);
+
+        in.clear();
+    }
+    return in;    
+ }
