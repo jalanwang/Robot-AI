ad50e8ef73ac5270a9d9dd391da1a82296d5effb
jalanwang <jalanwang@gmail.com>
2025-12-12 08:28:05 +0900

    구조 변경: main 함수에서 __name__ 출력 로직을 조건문으로 감싸기

--- Python/python3/module1/main.py
+++ Python/python3/module1/main.py
@@ -1,5 +1,6 @@
 import test
 
-print("#메인의 __name__ 출력하기")
-print(__name__)
-print()
+if __name__ == '__main__': #모듈을 들고 들어올 때, 이게 없으면 모듈도 실행한다.
+    print("#메인의 __name__ 출력하기")
+    print(__name__)
+    print()
