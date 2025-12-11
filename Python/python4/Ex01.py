0aea4107f660872f5fef0c58cfb6b136617508e6
jalanwang <jalanwang@gmail.com>
2025-12-12 08:28:22 +0900

    주석 추가: main 함수에서 __name__ 변수의 동작 설명 보강

--- Python/python4/Ex01.py
+++ Python/python4/Ex01.py
@@ -4,6 +4,8 @@ class Student:
         print()
 
 if __name__=='__main__':
+    # python ./thisfile.py 이러면 이 파일이 __name__ 변수가 __main__으로 바뀐다.
+    # import ./thatfile이 있으면 thatfile은 __main__ 값을 가지지 못한다. __name__ == thatfile
     student=Student()
     student.printName()
 
