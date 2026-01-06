a68696f5ad7c6ab35cddff5212dbe41ea92eaf2f
jalanwang <jalanwang@gmail.com>
2026-01-07 08:16:59 +0900

    pycharm 커널을 깔고 vscode에서 ipynb 파일을 다룰 수 있도록 설정함

--- deeplearning/basic1/example1.py
+++ deeplearning/basic1/example1.py
@@ -1,5 +1,10 @@
-import numpy as np
+""" import numpy as np
 a=[2,3,4]
 np_a=np.array(a)
 np_a=np_a*10
-print(np_a)
+print(np_a) """
+
+import sys
+print(sys.executable)
+import numpy as np
+print("Numpy version:", np.__version__)
