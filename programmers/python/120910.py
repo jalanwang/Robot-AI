9a6b6807901a74d6d5f8f42ea39e65609e7d9c4c
jalanwang <jalanwang@gmail.com>
2026-01-08 08:56:30 +0900

    세균 증식 문제의 solution 함수 수정: 반복문을 사용하여 t시간 후 세균 수를 계산하도록 변경

--- programmers/python/120910.py
+++ programmers/python/120910.py
@@ -3,8 +3,10 @@
 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때
 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.'''
 def solution(n, t):
-    answer=n*2*t     
-    return solution(n,t)
+    
+    for i in range(t):
+        n=2*n
+    return(n)
 
 if __name__ == '__main__':
     print(solution(2,10))
