7a26fb869bb42eebe9a37ab7ebb94213531a3c6f
jalanwang <jalanwang@gmail.com>
2026-04-26 08:36:12 +0900

    struct typedef 복습 및 + 연산자 사용할 때 자동형변환으로 인한 오류를 제거함

--- CPP/basic1/mytest02.cpp
+++ CPP/basic1/mytest02.cpp
@@ -6,34 +6,44 @@
 
 class Knight
 {
-public:
+private:
     //1. 멤버변수
-    int level;
-    std::string name;
-    int hp;
-    int mp;
+    int m_level;
+    std::string m_name;
+    int m_hp;
+    int m_mp;
     
+public:
     //2. 생성자
-    Knight(): level(1), name("기사"), hp(100), mp(50) {}
+    Knight(): m_level(1), m_name("기사"), m_hp(100), m_mp(50) {}
     Knight(std::string name, int level=1, int hp=100, int mp=50) {
-        this->level = 1;
-        this->name = name;
-        this->hp = 100;
-        this->mp = 50;        
+        m_level = level;
+        m_name = name;
+        m_hp = hp;
+        m_mp = mp;        
     }
+    //getter, setter
+    std::string getName() { return m_name; }
+    int getLevel() { return m_level; }
+    int getHp() { return m_hp; }
+    int getMp() { return m_mp; }
+    void setName(std::string name) { m_name = name; }
+    void setLevel(int level) { m_level = level; }
+    void setHp(int hp) { m_hp = hp; }
+    void setMp(int mp) { m_mp = mp; }
 
     //3. 멤버메소드
     std::string attack() {
-        return(name + "이(가) 손과 발로 공격!n");
+        return(m_name + "이(가) 손과 발로 공격!n");
     }
     std::string attack(std::string weapon) {
-        return(name + "이(가) " + weapon + " 공격!");
+        return(m_name + "이(가) " + weapon + " 공격!");
     }
     std::string attack(std::string weapon, std::string aura) {
-        return(name + "이(가) " + weapon + "에" + aura + "을(를) 감싼 후 공격!");
+        return(m_name + "이(가) " + weapon + "에" + aura + "을(를) 감싼 후 공격!");
     }
     std::string eat() {
-        return(name + "이(가) 먹는다.! \n");
+        return(m_name + "이(가) 먹는다.! \n");
     }
 };
 
@@ -41,10 +51,10 @@ int main()
 {
     //Knight 클래스 타입의 객체(인스턴스) 생성
     Knight k1("제임스", 1, 100, 100);
-    std::cout << "이름: " << k1.name << std::endl;
-    std::cout << "레벨: " << k1.level << std::endl;
-    std::cout << "체력: " << k1.hp << std::endl;
-    std::cout << "마력: " << k1.mp << std::endl << std::endl;
+    std::cout << "이름: " << k1.getName() << std::endl;
+    std::cout << "레벨: " << k1.getLevel() << std::endl;
+    std::cout << "체력: " << k1.getHp() << std::endl;
+    std::cout << "마력: " << k1.getMp() << std::endl << std::endl;
     std::cout << k1.attack() << std::endl;
     std::cout << k1.attack("용살검") << std::endl;  
     std::cout << k1.attack("용살검", " 마기") << std::endl;  
--- CPP/basic1/struct_test.cpp
+++ CPP/basic1/struct_test.cpp
@@ -16,12 +16,12 @@ int main()
     StudentInfo si1={'O', 20031128, 3.5};
     StudentInfo si2={'A', 19961128, 2.3};
     
-    std::cout << si1.bloodType + ' ';
-    std::cout << si1.stdNumber + ' ';
+    std::cout << si1.bloodType << ' ';
+    std::cout << si1.stdNumber << ' ';
     std::cout << si1.grade << std::endl;
 
-    std::cout << si2.bloodType + ' ';
-    std::cout << si2.stdNumber + ' ';
+    std::cout << si2.bloodType << ' ';
+    std::cout << si2.stdNumber << ' ';
     std::cout << si2.grade << std::endl;
 
     return 0;
